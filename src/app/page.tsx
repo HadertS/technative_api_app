140c1ec57fb664a91eb02d9cae43e9e312618116
HadertS <source.zero@googlemail.com>
2025-09-03 17:37:28 +0100

    fixed implicit typing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+interface post {
+  id: number;
+  title: string;
+}
+
 export default async function Page() {
   const data = await fetch("https://api.vercel.app/blog");
   const posts = await data.json();
   return (
     <ul>
-      {posts.map((post) => (
+      {posts.map((post:post) => (
         <li key={post.id}>{post.title}</li>
       ))}
     </ul>
